28e6c8e78194a2bbc8de18a96b04e19738b0f19a
Maryess <vip.ivan.ivanov2003@bk.ru>
2024-12-11 01:16:54 +0300

    changed page home

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,9 +1,35 @@
-import { Diagram } from '../../ui/diagram/Diagram';
+import { useState } from "react";
+import Button from "../../ui/button/Button";
+import { Diagram } from "../../ui/diagram/Diagram";
 
 export const Home = () => {
+	const [close, setClose] = useState(false);
+
+	const component = [
+		{
+			title: "Диаграмма",
+		},
+	];
+
 	return (
 		<div>
-			<Diagram />
+			<header>
+				<span>Select component</span>
+				{component.map((item, index) => {
+					return (
+						<div key={index}>
+							<button
+								onClick={() => {
+									setClose(!close);
+								}}
+							>
+								{item.title}
+							</button>
+							{close ? <Diagram /> : <Button text="click" />}
+						</div>
+					);
+				})}
+			</header>
 		</div>
 	);
 };
